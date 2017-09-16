f9dff7c5477a26c57cad269921cf72565e330d57
YJBeetle <yjbeetle@gmail.com>
2017-09-16 16:57:41 +0800

    gv

---
+++ gv/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#请注意使用UTF-8 without BOM格式，否则可能会出错
+#脚本 by Sunbread
+########## Setting ##########
+threads=16 # 根据自身情况调整，若不会调整则按默认值即可
+########## Code ##########
+echo 伪多线程脚本 by Sunbread
+for((i=0;i<$threads;++i));do while :;do
+	    [curl ...] >/dev/null 2>/dev/null
+	        # 用刚才的命令替换中括号里的内容（包括两个中括号本身）
+	done &
+done
+echo 结束脚本请使用Ctrl+C，使用其它方法可能会出现无法预料的异常，谢谢合作
+wait
+
